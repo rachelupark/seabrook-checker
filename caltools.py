6627ea1805dae7384355265bbf0701cad7c706ba
personal <rachelupark@protonmail.com>
2021-06-24 13:22:47 -0700

    created skeleton for price functionality

--- caltools.py
+++ caltools.py
@@ -8,10 +8,9 @@ from selenium import webdriver
 import time
 from navtools import scroll_down
 
-# takes a date in mm/dd format and a driver for a single house's info
+# accepts a date in mm/dd format and a driver for a single house's info
+# returns whether the house is available on that day
 def get_av(date: str, driver):
-    # gets the overnight price of a given date
-    # print("Here's the date you gave me: " + date) #hint
     scroll_down(driver)
     mm = date.split("/")[0]
     dd = date.split("/")[1]
@@ -22,22 +21,38 @@ def get_av(date: str, driver):
         if get_mm(x) == mm:
             chosenmonthdriver = x
             break
-    # print("Here is the month of the driver I matched to your request:") #hint
-    # print(get_mm(chosenmonthdriver)) #hint
-    # print("Here's the day I heard: " + dd) #hint
-    # print("Here's the availability of the day you selected: ") #hint
+
     availability = get_av_day(dd, chosenmonthdriver)
-    # print(availability) #hint
+
     if availability == "day av-O" or availability == "day av-O av-IN":
         return True
     else:
         return False
+
+def get_price(date: str, driver):
+    # return the price of that night as an int
+    return
+
+
+######################################################################
+## These methods are only for caltools. Please don't import them.
+
+def find_month(date, driver):
+    # return web elements corresponding to the selected month.
+    return
+
 
 # Accepts a day and an elements object of a specific month.
 def get_av_day(dd: str, monthele):
     dayele = monthele.find_elements_by_class_name('day')[int(dd)-1]
     status = dayele.get_attribute('class')
     return status
+
+def get_price_day(dd: str, monthele):
+    dayele = monthele.find_elements_by_class_name('rc-price')[int(dd)-1]
+    price = dayele.get_attribute('innerHTML')
+    price = "".join(price[1:].split(","))
+    return int(price)
 
 # Accepts elements object of a specific month.
 def get_mm(monthdriver):
@@ -46,6 +61,8 @@ def get_mm(monthdriver):
     mm = month_to_mm(monthyeararr.split("&nbsp;")[0])
     return mm
 
+
+## Simple formatting and conversion methods.
 def month_to_mm(month: str):
     if month == "January":
         return "01"
